3d476d662e548df9d34fb515d6279bc7890538d1
laufofbread <hello@lauramccartney.co.uk>
2017-10-26 15:13:00 +0100

    Split edit form into components

--- src/EditRemove.js
+++ src/EditRemove.js
@@ -1,55 +1,88 @@
 import React from 'react';
 import firebase from './firebase.js';
 
-class EditRemove extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      items: []
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+const ImageList = (props) => {
+    console.log(props);
 
-  handleChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
+    let items = props.items.map((item) => {
+        return (
+            <ImageListItem key={item.id} />
+        )
     });
-  }
+    return (
+        <ul className="edit-form-list">
+            {items}
+        </ul>
+    );
+};
 
-  handleSubmit(e) {
-    e.preventDefault();
-    const itemsRef = firebase.database().ref('items');
-    const item = {
-        image: this.state.image,
-        title: this.state.title,
-        filter: this.state.filter,
-        date: this.state.date
+class ImageListItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleClick.bind(this);
+    }
+    handleClick() {
+        let index = parseInt(this.props.index);
+        this.props.openCloseForm(index);
+    }
+    removeFromList() {
+        let id = parseInt(this.props.id);
+        this.props.removeItem(id);
+    }
+    render () {
+        return (
+            <li className="edit-form-list-item">
+                <h3>{this.props.title}</h3>
+                <button onClick={this.handleClick}>Remove</button>
+                <section style={{display: this.props.item.formOpen === true ? 'block' : 'none'}}>
+                    <label>Are you sure?</label>
+                    <button onClick={() => this.removeFromList(this.props.item.id)}>Remove</button>
+                    <button onClick={this.handleClick}>Cancel</button>
+                </section>
+            </li>
+        )
+    }
+}
+
+class EditRemoveList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.openCloseForm = this.openCloseForm.bind(this);
+    this.removeItem = this.removeItem.bind(this);
+    this.state = { items: [] };
+  }
+  componentDidMount() {
+      const itemsRef = firebase.database().ref('items');
+      itemsRef.on('value', (snapshot) => {
+          let items = snapshot.val();
+          let newState = [];
+          for (let item in items) {
+              newState.push({
+                  id: item,
+                  image: items[item].image,
+                  title: items[item].title,
+                  filter: items[item].filter,
+                  date: items[item].date,
+                  formOpen: false
+              });
+          }
+          this.setState({
+              items: newState
+          });
+      });
     };
-    itemsRef.push(item);
-    this.setState({
-        image: "",
-        title: "",
-        filter: "",
-        date: new Date().getTime()
-    });
-  }
-
+    openCloseForm(index) {
+        this.items[index].formOpen = !this.items[index].formOpen;
+        this.setState({items: this.items});
+    };
+    removeItem(itemId) {
+        const itemRef = firebase.database().ref(`/items/${itemId}`);
+        itemRef.remove();
+    };
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>Image</label>
-        <input type="file" name="image" value={this.state.image} onChange={this.handleChange} />
-
-        <label>Title</label>
-        <input type="text" name="title" value={this.state.title} onChange={this.handleChange} />
-
-        <label>Filter</label>
-        <input type="text" name="filter" value={this.state.filter} onChange={this.handleChange} />
-
-        <button type="submit">Upload</button>
-      </form>
+        <ImageList items={this.state.items} openCloseForm={this.openCloseForm} removeItem={this.removeItem} />
     );
   }
 }
-export default EditRemove;
+export default EditRemoveList;
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import UploadForm from './UploadForm';
+import EditRemoveList from './EditRemove';
 
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
-    React.createElement(UploadForm),
+    <section>
+      <UploadForm/>
+      <EditRemoveList/>
+    </section>,
     document.getElementById('mount')
   );
 });
