9ff67fb3549da2f6a26a1c1976fb9dcc9e2155ff
Laura McCartney <hello@lauramccartney.co.uk>
2019-01-23 17:06:48 +0000

    Style about section

--- src/about/About.js
+++ src/about/About.js
@@ -1,17 +1,20 @@
 import React from 'react';
 
 class About extends React.Component {
-  constructor() {
-    super();
+  constructor() { super(); }
 
-  }
-  componentDidMount() {
-
-  };
-render () {
+  render () {
     return (
-      <main>
-
+      <main className="body-wrapper">
+        <section className="text-block">
+          <p>
+            Hello I&#39;m <a href="//lauramccartney.co.uk">Laura McCartney</a>.
+          </p>
+          <p>
+            This is my time capsule to a period of 13 months travelling the world.
+            I took over 4000 photographs; selecting, editing and uploading as I went along.
+          </p>
+        </section>
       </main>
     )
   }
