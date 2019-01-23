e40628c00574df66494c510c723ce4711f614b33
Laura McCartney <hello@lauramccartney.co.uk>
2019-01-23 17:29:26 +0000

    Add another paragraph to the about section

--- src/about/About.js
+++ src/about/About.js
@@ -14,6 +14,9 @@ class About extends React.Component {
             This is my time capsule to a period of 13 months travelling the world.
             I took over 4000 photographs; selecting, editing and uploading as I went along.
           </p>
+          <p>
+            All of my photographs were taken with an Olympus OM-D EM10 Mark II with a M.Zuiko Digital 25mm f/1.8 lens. All editing was done using the VSCO app for iPhone.
+          </p>
         </section>
       </main>
     )
